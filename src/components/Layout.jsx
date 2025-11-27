b97401c46bc58cd14dee1aca050396164d4f2636
Kaushal Bhattarai <kb147181@gmail.com>
2025-11-27 12:52:26 -0600

    Enhance Hero component with training program details and rules; fix typo in Layout component

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,7 +5,7 @@ export default function Layout(props){
     const header = (
         <header>
             <h1 className="text-gradient">The FIT30</h1>
-            <p><strong>Your 30 Days Simple Workouts Program</strong></p>
+            <p><strong>Your 30 Days Simple Workout Program</strong></p>
         </header>
     )
 
