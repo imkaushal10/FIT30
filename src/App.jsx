1f5859f21bdb0d4add3c9983328c1dfaddad1296
Kaushal Bhattarai <kb147181@gmail.com>
2025-11-26 19:32:20 -0600

    Add Hero component and integrate it into App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Layout from './components/Layout.jsx'
 import Grid from './components/Grid.jsx'
+import Hero from './components/Hero.jsx'
 
 function App() {
   return (
 
       <Layout>
         <main>
+          <Hero />
           <Grid />
         </main>
       </Layout>
