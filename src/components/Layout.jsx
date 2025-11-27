082e5564b3dc24003f38a1f7366dfb8fc1d16d06
Kaushal Bhattarai <kb147181@gmail.com>
2025-11-26 19:20:05 -0600

    Implement Layout component with header, footer, and children rendering

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,25 @@
-export default function Layout(){
+export default function Layout(props){
+    
+    const { children } = props
+
+    const header = (
+        <header>
+            <h1 className="text-gradient">The FIT30</h1>
+            <p><strong>Your 30 Days Simple Workouts Program</strong></p>
+        </header>
+    )
+
+    const footer = (
+        <footer>
+            <p>Built by <a href="" target="_blank">Kaushal Bhattarai</a></p>
+        </footer>
+    )
+    
     return (
-        <></>
+        <>
+            {header}
+            {children}
+            {footer}
+        </>
     );
 }
