cff9e611c33916ae5698c81dba123c7f8261ce34
Kaushal Bhattarai <kb147181@gmail.com>
2025-11-30 18:17:14 -0600

    Update Modal component to include exercise name and description display

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -8,6 +8,16 @@ export default function Modal(props) {
     return ReactDom.createPortal((
         <div className='modal-container'>
             <button className='modal-underlay' onClick={handleCloseModal} />
+            <div className='modal-content'>
+                <div>
+                    <h6>Name</h6>
+                    <h2 className='skill-name'>{name.replaceAll('-', ' ')}</h2>
+                </div>
+                <div>
+                    <h6>Description</h6>
+                    <p>{description}</p>
+                </div>
+            </div>
         </div>
     ),
         document.getElementById('portal')
