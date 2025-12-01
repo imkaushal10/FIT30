f52a1a1802858e568e513f73ee48a488b440d981
Kaushal Bhattarai <kb147181@gmail.com>
2025-11-30 18:15:34 -0600

    Implement Modal component with exercise description and close functionality

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,5 +1,16 @@
-export default function Modal(){
-    return (
-        <></>
-    );
+import ReactDom from 'react-dom'
+
+export default function Modal(props) {
+
+    const { showExerciseDescription, handleCloseModal } = props
+    const { name, description } = showExerciseDescription || {}
+
+    return ReactDom.createPortal((
+        <div className='modal-container'>
+            <button className='modal-underlay' onClick={handleCloseModal} />
+        </div>
+    ),
+        document.getElementById('portal')
+    )
+
 }
